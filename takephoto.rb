835a7dbe525cc0600e636c2008239859018a9bc4
weaver <theandrewjw@gmail.com>
2019-10-31 13:52:02 -0700

    style and content

--- takephoto.rb
+++ takephoto.rb
@@ -1,16 +1,25 @@
 #!/usr/bin/ruby
+# frozen_string_literal: true
+
+Script_dir = __dir__
 
 if RUBY_PLATFORM.include?('linux')
-  Linux = true
+  LINUX = true
 else
   RUBY_PLATFORM.include?('some mac thing')
   MAC = true
 end
 ffmpeg_device_options = []
 
-if Linux
-  ffmpeg_device_options += [ '-f', 'v4l2', '-i' ,'/dev/video0' ]
-end
+def run_camera(mode)
+  if LINUX
+    ffmpeg_device_options += ['-f', 'v4l2', '-i', '/dev/video0']
+  elsif MAC
+    ffmpeg_device_options += ['-f', 'avfoundation', '-i', 'default']
+  end
 
-ffplay_command = [ 'ffplay', ffmpeg_device_options ].flatten
-system(*ffplay_command)
+  if mode == 'ffplay'
+    ffplay_command = ['ffplay', ffmpeg_device_options].flatten
+    system(*ffplay_command)
+  end
+end
