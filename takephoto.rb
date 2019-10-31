0a6822f874a1cd05cb2272621892dfa482f5ccbb
weaver <theandrewjw@gmail.com>
2019-10-30 17:08:43 -0700

    start microservice

--- takephoto.rb
+++ takephoto.rb
@@ -1,1 +1,16 @@
 #!/usr/bin/ruby
+
+if RUBY_PLATFORM.include?('linux')
+  Linux = true
+else
+  RUBY_PLATFORM.include?('some mac thing')
+  MAC = true
+end
+ffmpeg_device_options = []
+
+if Linux
+  ffmpeg_device_options += [ '-f', 'v4l2', '-i' ,'/dev/video0' ]
+end
+
+ffplay_command = [ 'ffplay', ffmpeg_device_options ].flatten
+system(*ffplay_command)
