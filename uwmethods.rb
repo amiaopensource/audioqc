a8de3f7ca1098bba2ccd05967ef6e3aec47392ce
weaver <theandrewjw@gmail.com>
2019-11-01 11:43:45 -0700

    rubocop

--- uwmethods.rb
+++ uwmethods.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Iterator
   def initialize(value)
     @value = value
@@ -5,7 +7,7 @@ class Iterator
 
   def increase
     @value += 1
-    "%02d" % [@value]
+    format('%02d', @value)
   end
 end
 
@@ -21,8 +23,13 @@ class Media_Object
   def initialize(value)
     @system = get_os
     @input_path = value
-    if File.file?(@input_path)
-      @input_is_file = true
+    if @system == 'linux' || @system == 'mac'
+      mime_type = `file -b --mime-type #{@input_path}`.strip
+    end
+    if File.file?(@input_path) && mime_type.include?('audio')
+      @input_is_audio = true
+    elsif File.file?(@input_path) && mime_type.include?('video')
+      @input_is_video = true
     elsif File.directory?(@input_path)
       @input_is_dir = true
     else
@@ -30,7 +37,14 @@ class Media_Object
     end
   end
 
-
+  def make_derivative
+    root_name = File.basename(@input_path, '.*')
+    out_dir = File.dirname(@input_path)
+    if @input_is_audio
+      output = "#{out_dir}/#{root_name}.flac"
+      system('ffmpeg', '-i', @input_path.to_s, '-c:a', 'flac', output.to_s)
+    end
+  end
 
   def test
     if @input_is_file
